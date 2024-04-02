c365b5bceca587fd76ddf0b7599bfe8602993cd2
merseyalma <merseyalma@gmail.com>
2024-04-02 13:03:58 +0800

    fix: fix cors

---
+++ src/main/java/com/zcunsoft/cfg/CorsConfiguration.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.zcunsoft.cfg;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author mersey
+ *
+ */
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*")
+				.allowedHeaders("x-requested-with", "accept", "authorization", "content-type")
+				.exposedHeaders("access-control-allow-headers", "access-control-allow-methods",
+						"access-control-allow-origin", "access-control-max-age", "X-Frame-Options")
+				.allowCredentials(true).maxAge(3600);
+	}
+
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry)
+//	{
+//		registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/**");
+//	}
+}

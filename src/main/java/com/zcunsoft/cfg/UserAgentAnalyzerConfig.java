7e012a351e8a09c83f3d43349e93e9a8458dc546
merseyalma <merseyalma@gmail.com>
2024-06-28 13:01:25 +0800

    perf: 升级ua依赖

--- src/main/java/com/zcunsoft/cfg/UserAgentAnalyzerConfig.java
+++ src/main/java/com/zcunsoft/cfg/UserAgentAnalyzerConfig.java
@@ -1,6 +1,7 @@
 package com.zcunsoft.cfg;
 
 import nl.basjes.parse.useragent.AbstractUserAgentAnalyzer;
+import nl.basjes.parse.useragent.UserAgent;
 import nl.basjes.parse.useragent.UserAgentAnalyzer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,12 @@ import org.springframework.context.annotation.Configuration;
 public class UserAgentAnalyzerConfig {
 	@Bean
 	public AbstractUserAgentAnalyzer userAgentAnalyzer() {
-		UserAgentAnalyzer userAgentAnalyzer = UserAgentAnalyzer.newBuilder().hideMatcherLoadStats().withCache(10000)
+		UserAgentAnalyzer userAgentAnalyzer = UserAgentAnalyzer.newBuilder().withField(UserAgent.AGENT_NAME)
+				.withField(UserAgent.AGENT_NAME_VERSION)
+				.withField(UserAgent.DEVICE_NAME)
+				.withField(UserAgent.DEVICE_BRAND)
+				.withField(UserAgent.OPERATING_SYSTEM_NAME)
+				.withField(UserAgent.OPERATING_SYSTEM_NAME_VERSION).hideMatcherLoadStats().withCache(10000)
 				.build();
 
 		return userAgentAnalyzer;
